cae98771ef70cb5234bf1d9b41efa80e37c45872
王文彩(10029783) <wangwc@glodon.com>
2020-07-27 20:13:23 +0800

    git测试提交

---
+++ src/Ship.java
@@ -0,0 +1,19 @@
+public class Ship {
+    public static void main(String[] args) {
+        f();
+    }
+
+    static void g() throws FooAException {
+        System.out.println("抛出A型异常!");
+        throw new FooAException();
+    }
+
+    static void f() {
+        try {
+            g();
+        }catch (FooAException fae) {
+            System.out.println("捕获A异常，并抛出运行时异常B!");
+            throw new FooBException();
+        }
+    }
+}
